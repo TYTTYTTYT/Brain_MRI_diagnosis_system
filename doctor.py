e3a2890da39e2207b3025f5d8b5c7d173c9c2bf7
TYTTYTTYT <tai.yintao@gmail.com>
2021-07-27 18:35:04 +0800

    update

--- doctor.py
+++ doctor.py
@@ -4,20 +4,17 @@ from io import BytesIO
 
 # %%
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
+    file_idx = 0
+    message = "Hi, don't worry, you are fine!"
     def do_GET(self):
         self.send_response(200)
         # self.send_header('hh', 'awf')
         self.end_headers()
         response = BytesIO()
-        with open('model_example', 'rb') as model:
-            response.write(model.read())
+        response.write(SimpleHTTPRequestHandler.message)
         self.wfile.write(response.getvalue())
-        print(self.headers)
-        print(self.path)
         print("Incomming address", self.client_address)
         content = self.rfile.read()
-        print(content)
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -29,7 +26,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'Received: ')
         response.write(body)
         self.wfile.write(response.getvalue())
+        print(len(body))
+        print(type(body))
+        print(body)
+
+        with open('image/' + str(self.file_idx), 'wb') as file:
+            SimpleHTTPRequestHandler.file_idx += 1
+            file.write(body)
 
 # %%
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 2095), SimpleHTTPRequestHandler)
 httpd.serve_forever()
--- image_receiver.py
+++ image_receiver.py
@@ -4,6 +4,7 @@ from io import BytesIO
 
 # %%
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    file_idx = 0
 
     def do_GET(self):
         self.send_response(200)
@@ -29,7 +30,14 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         response.write(b'Received: ')
         response.write(body)
         self.wfile.write(response.getvalue())
+        print(len(body))
+        print(type(body))
+        print(body)
+
+        with open('image/' + str(self.file_idx), 'wb') as file:
+            SimpleHTTPRequestHandler.file_idx += 1
+            file.write(body)
 
 # %%
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('', 2095), SimpleHTTPRequestHandler)
 httpd.serve_forever()
